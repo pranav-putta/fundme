fb197de7cbe208544b637571788133ac58cb9bcb
Pranav Putta <pranav.putta22@gmail.com>
2016-11-18 11:54:24 -0600

    initial push

---
+++ android/FundMe/app/src/main/java/net/codealizer/fundme/ui/launch/LaunchActivity.java
@@ -0,0 +1,15 @@
+package net.codealizer.fundme.ui.launch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.codealizer.fundme.R;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
